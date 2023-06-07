f504706333515195cda38875f925231234a0f8da
Josh Santos <josh@omnidapps.com>
2023-06-07 20:41:50 +0700

    Add tests for user aggregation

--- wg/pkg/aggregation/users.go
+++ wg/pkg/aggregation/users.go
@@ -3,6 +3,7 @@ package aggregation
 import (
 	"concurrency-poc/pkg/models"
 	"context"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -12,43 +13,81 @@ type (
 		GetUserName(*sync.WaitGroup)
 		GetFriends(*sync.WaitGroup)
 		Read() models.User
+		Done()
 	}
 
 	UserData struct {
-		name    chan string
-		friends chan []models.User
+		Name    chan string
+		Friends chan []models.User
 	}
 )
 
 func NewUserData() UserData {
 	return UserData{
-		name:    make(chan string, 1),
-		friends: make(chan []models.User, 1),
+		Name:    make(chan string, 1),
+		Friends: make(chan []models.User, 1),
 	}
 }
 
 func (u *UserData) GetUserName(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	u.name <- getUserData().Name
+	u.Name <- getUserData().Name
 }
 
 func (u *UserData) GetFriends(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	u.friends <- getUserFriends()
+	u.Friends <- getUserFriends()
 }
 
+// Done allows you to specify when you are finished aggregating.
+// It will close any channels which don't yet have data, ensuring
+// subsequent Read does not block.
+func (u *UserData) Done() {
+	v := reflect.ValueOf(*u)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i).Interface()
+		possibleChan := reflect.ValueOf(field)
+		var isChannel = possibleChan.Kind() == reflect.Chan
+		if !isChannel {
+			continue
+		}
+
+		if possibleChan.Len() == 0 {
+			possibleChan.Close()
+		}
+	}
+}
+
+// Read aggregates all data that was prepared for consumption.
 func (u UserData) Read() (user models.User) {
-	var received int = 0
-	for received != 2 {
+	// FIXME: This has two main caveats currently: the complexity scales with
+	// the amount of data in the struct, and it only supports reading from
+	// channels of one element
+	for {
 		select {
-		case friends := <-u.friends:
-			user.Friends = friends
-			received++
-		case name := <-u.name:
-			user.Name = name
-			received++
+		case friends, ok := <-u.Friends:
+			if !ok {
+				u.Friends = nil
+			} else {
+				user.Friends = friends
+				close(u.Friends)
+			}
+		case name, ok := <-u.Name:
+			if !ok {
+				u.Name = nil
+			} else {
+				user.Name = name
+				close(u.Name)
+			}
+		default:
+			continue
+		}
+
+		if u.Name == nil && u.Friends == nil {
+			break
 		}
 	}
 
